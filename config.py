c8f0a50ed3811a4be88d64636e1080fbaac6f3ee
mary atieno <mercymary 948@gmail.com>
2022-05-12 10:24:11 +0300

    updates track modification

---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    debug = True
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mary:mary@localhost/pitch'
+
+    #  email configurations
+    MAIL_SERVER = 'kima.gmail.com'
+    MAIL_PORT = 567
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mary:mary@localhost/pitch'
+
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+
+}
